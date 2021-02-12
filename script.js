ebde71b05c5021125c86d4f70482e0e40aa78f43
Tamal Biswas <tamalbiswas32@gmail.com>
2021-02-12 22:11:04 +0530

    Publishing my first website

---
+++ script.js
@@ -0,0 +1,17 @@
+const preLoader = document.getElementById('loader');
+
+function loadingDone(){
+    preLoader.style.display = 'none';
+}
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
